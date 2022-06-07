3a651e6d5c9d2e658292615d5efd756ea733a815
dhung1308 <dhung1308@gmail.com>
2022-06-07 15:23:23 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,32 @@
-<?php 
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=320, initial-scale=1.0, maximum-scale=1.0"/>
+        <title>ZingPlay</title>
 
-$state = "";
-$code = "";
-$token = "";
-$user = "";
-
-if (isset($_REQUEST['state']))
-    $state = $_REQUEST['state'];
-
-if (isset($_REQUEST['code']))
-    $code = $_REQUEST['code'];
-
-    
-if (isset($_REQUEST['id_token']))
-    $token = $_REQUEST['id_token'];
-
-
-if (isset($_REQUEST['user']))
-    $user = $_REQUEST['user'];
-
-echo "</br>State ".$state;
-echo "</br>Code ".$code;
-echo "</br>Token ".$token;
-echo "</br>User ".$user;
-?>
+        <?php 
+            $state = "";
+            $code = "";
+            $token = "";
+            $user = "";
+            if (isset($_REQUEST['state']))
+                $state = $_REQUEST['state'];
+            if (isset($_REQUEST['code']))
+                $code = $_REQUEST['code'];
+            if (isset($_REQUEST['id_token']))
+                $token = $_REQUEST['id_token'];
+            if (isset($_REQUEST['user']))
+                $user = $_REQUEST['user'];
+        ?>
+        <script>
+            function callback() {
+                var token = <?php echo $token;?>;
+                console.log("Token : " + token)
+                window.location = "zpslogin:://zpslogin/" + token;
+            }
+        </script>
+    </head>
+    <body onload="callback()">
+		<h1>You are Login Success !!!</h1>	    
+    </body>
+</html>
